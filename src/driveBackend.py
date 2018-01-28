2a5a69cdf1e1b7965b66fa3a846374fd15e99c37
Dale Ferrario <daleferrario@gmail.com>
2018-01-28 15:21:05 -0800

    accidentally added yaml lines to driveBackend

--- src/driveBackend.py
+++ src/driveBackend.py
@@ -4,7 +4,6 @@ import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import ntpath
-import yaml
 # Local imports
 from abstractBackend import abstractBackend
 
@@ -12,7 +11,6 @@ from abstractBackend import abstractBackend
 class driveBackend(abstractBackend):
     def __init__(self, config):
         super().__init__()
-        settings = yaml.a
         gauth = GoogleAuth(settings_file=os.path.join(config['drive.google.com']['drive_config_dir'], config['drive.google.com']['auth_config_filename']))
         self.drive_connection = GoogleDrive(gauth)
         print("Google Drive login!")
